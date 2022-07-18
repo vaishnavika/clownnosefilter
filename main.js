40ff0ed54741fa06d58a2f196028a6399d557c4e
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-07-18 16:55:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=creatcanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
