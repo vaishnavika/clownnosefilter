ecff0846ee5ab6a0cbdff079a740efcfba27f692
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-08-01 16:45:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
-function preload(){
+ noseX=0;
+ noseY=0;
+  
+  function preload(){
+    clown_nose=loadImage('clown.png');
 
 }
 
 function setup(){
-    canvas=creatcanvas(300,300);
+    canvas = createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoad);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoad(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x="+ results[0].pose.nose.x);
+        console.log("nose y="+ results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-
-}
-
-function draw(){
+ image(video, 0,0,300,300);
+ image(clown_nose,noseX,noseY,30,30);
 
 }
 
 function take_snapshot(){
     save('myFilterImage.png');
 }
+
